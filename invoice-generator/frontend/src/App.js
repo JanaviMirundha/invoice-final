27499e435d7cc280bbd70ee953450cbe40994ead
sece <tmkarthickraja@gmail.com>
2025-12-15 21:58:08 +0530

    Test commit

--- invoice-generator/frontend/src/App.js
+++ invoice-generator/frontend/src/App.js
@@ -8,6 +8,7 @@ import Dashboard from './pages/Dashboard';
 import CreateInvoice from './pages/CreateInvoice';
 import InvoicePreview from './pages/InvoicePreview';
 import About from './pages/About';
+// test commit
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
